458c05427bfdc94d97daa6b1fd34943aec21e779
PWashburn55 <pw325555@gmail.com>
2020-03-05 09:04:41 +0000

    add hyperlinks

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,18 +6,21 @@ function initMap() {
                     lat: 59.334591,
                     lng: 18.063240
                 }
-        })
+        });
 
         var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         var locations = [
-        { lat: 59.3539749, lng: 18.1036816 },
-        { lat: 59.3194085, lng: 18.0682444 },
-        { lat: 59.3189195, lng: 18.0592735 },
-        { lat: 59.3189727, lng: 18.0601436 },
-        { lat: 59.3374381, lng: 18.0588704 },
-        { lat: 59.3383229, lng: 18.0504655 },
-        { lat: 59.3383229, lng: 18.0504655 }
+        { lat: 59.3538998, lng: 18.1055148 }, // myrorna kolargatan
+        { lat: 59.3193874, lng: 18.0700677 }, // emmaus
+        { lat: 59.3188953, lng: 18.0615576 }, // stadsmissionen
+        { lat: 59.3191879, lng: 18.0620237 }, // röda körset
+        { lat: 59.3174722, lng: 18.049938 }, // judit
+        { lat: 59.3173322, lng: 18.0495676 }, // filippa k
+        { lat: 59.3182002, lng: 18.0488125 }, // beyond retro zinken
+        { lat: 59.3133187, lng: 18.0814983 }, // lisa larsson
+        { lat: 59.3383229, lng: 18.0526542 }, // zak
+        { lat: 59.3374381, lng: 18.0588704 } // myrorna kyrkogatan
     ];
 
     var markers = locations.map(function(location,i) {
